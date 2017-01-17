d174f9680d90503eda7d3445433398db03b88a85
Stephen Edwards <stephen.edwards@foostix.lu>
2017-01-16 21:38:37 -0800

    add function to check all groups without scheduler

--- lib/aws-elastigroup.rb
+++ lib/aws-elastigroup.rb
@@ -78,6 +78,12 @@ module AWSElastigroup
   end
   module_function :aws
   
+  def check_groups
+    AWSElastigroup.config.groups.each do |group|
+    #Check group spot status: true = ok, false = instances running < desired capacity
+    puts "Checking group #{group.name}"
+    group.check_spot_status
+  end
   
   def run
     #client = @@aws::AutoScaling::Client.new(:region => "eu-central-1")
